2c1f78ccaefbbd7dc3665df3fd3491f5a0799c7a
jamieAhn <jamie.ahn@nsuslab.com>
2023-10-06 19:26:07 +0900

    init

---
+++ deploy-helper/src/main/java/com/nsuslab/deploy/DeployHelperApplication.java
@@ -0,0 +1,13 @@
+package com.nsuslab.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployHelperApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/nsuslab/assistant/WorkAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.nsuslab.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkAssistantApplication.class, args);
+    }
+
+}
